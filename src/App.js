ec2350c6173c66287bbda47f1da23bc95491c81f
Prachi24599 <prachi24599@gmail.com>
2023-06-17 20:29:21 +0530

    Initial Stage - Components Created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Testimonials from "./components/Testimonials";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Ours Testimonials</h1>
+      <div></div>
+      <Testimonials />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Testimonials.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Card";
+
+const Testimonials = () => {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+};
+
+export default Testimonials;
