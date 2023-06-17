dd7ae81504c28755245d3fdcd57876654b12cc40
Prachi24599 <prachi24599@gmail.com>
2023-06-17 22:41:36 +0530

    seperate button in testimonial component as Card contains data of only one review

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
-import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
 
 const Card = (props) => {
   const review = props.review;
@@ -14,33 +14,25 @@ const Card = (props) => {
         />
         <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-20] left-[10px]"></div>
       </div>
+
       <div className="text-center mt-7">
         <p className="font-bold text-2xl capitalize">{review.name}</p>
       </div>
+
       <div className="text-center mt-7">
         <p className="text-violet-300 uppercase text-sm">{review.job}</p>
       </div>
+
       <div className="text-violet-400 mx-auto mt-5">
         <FaQuoteLeft />
       </div>
+
       <div className="text-center mt-4 text-slate-500">
         <p>{review.text}</p>
       </div>
+
       <div className="text-violet-400 mx-auto mt-5">
         <FaQuoteRight />
-      </div>
-      <div className="flex text-3xl mt-5 gap-3 text-violet-400 font-bold mx-auto">
-        <button className="cursor-pointer hover:text-violet-500">
-          <FiChevronLeft />
-        </button>
-        <button className="cursor-pointer hover:text-violet-500">
-          <FiChevronRight />
-        </button>
-      </div>
-      <div>
-        <button className="bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg">
-          Surprise Me
-        </button>
       </div>
     </div>
   );
